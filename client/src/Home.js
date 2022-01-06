1a18aad45b1ab4d108b5126a62504ec46d758ff9
ReshzevarmaRajendran <reshzevarma.rajendran@principal.com.my>
2022-01-06 13:22:19 +0800

    update home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,19 +5,30 @@ class Home extends Component {
   render() {
     return (
         <div className="App">
-			<p><strong>Welcome to Principal MiniProgram</strong></p>
-			<p>This POC is to demontrate the capabilities of Principal Web Application to integrate with TNGD eWallet mini program API</p>
-			<p>The scope of this POC:-
-			<ul>
-				<li>Principal Web Application <strong>integrate</strong> eWallet miniprogram information in industry <strong>token based authentication</strong></li>
-				<li>Principal Web Application <strong>encrypt/decrypt</strong> eWallet miniprogram information</li>
-				<li>Principal Web Application <strong>retrieve eKYC information</strong> from eWallet miniprogram</li>
-				<li>Principal Web Application <strong>submit payment information</strong> to eWallet miniprogram payment API</li>
-				<li>Principal Web Application <strong>receive payment status</strong> from eWallet miniprogram</li>
-				<li>Principal Web Application <strong>display payment status</strong></li>
-			</ul>
-			<strong>Note:</strong> This POC must be running inside TNGD sandbox mobile app.
-			</p>
+			<h2> Sample Mini Program Form!!</h2>
+            <div>
+                <label for ="name">Name</label>
+                <input type="text" name="name"  id="name" className="form-control"/>
+            </div>
+
+            <div>
+                <label for ="phone">Phone Number</label>
+                <input type="text" name="phone"  id="phone" className="form-control"/>
+            </div>
+
+            <div>
+                <label for ="email">Email</label>
+                <input type="text" name="email"  id="email" className="form-control"/>
+            </div>
+
+            <div>
+					<label for="dob">Date of Birth</label>
+					<input type="text" className="form-control" id="dob" name="dob"  />
+			</div>
+            <div>
+                    <br/>
+					<button className="btn btn-primary" >Next</button>
+            </div>
 		</div>
     );
   }
