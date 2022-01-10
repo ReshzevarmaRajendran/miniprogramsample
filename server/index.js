7b78c4aa9989d70060d11dad28365cfa4ea86fea
ReshzevarmaRajendran <reshzevarma.rajendran@principal.com.my>
2022-01-10 09:49:02 +0800

    Update server.js code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const path = require('path');
-const PORT = process.env.PORT || 3001;
 
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const clientId = '2171020067475894';
+var privateKeyStr = 'MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDvulYxWd48VJSjQK0sl+WZatRUoBCDHKk7ywky42ClKnikt10mACpv40r4jfKZ8NOSUuK8vb+PNjPqMx77PVCb/JnwJeC/NrJpqNbhy4jNgBW+2TmidTI35a6sFky6Uhm27mtXh+0DJ+ldpqVCdk3bFf9VX12Sd0svk8cNVoVA/TDkZYTfnQ44RKSFrnfgJt1El+9WoZRK7uOMB6M2jwoIVj6YRk/jSduPZ2NFZAMRO+a+ZpcmgNNx4ee6RSeeK7zUXTrVUEWbsx80qmYayISYAL60ji+kYIGgrn/+f+EYtgpbkvjbUwyIEgSub0FHLhE6waoS0Yg96xk4GrN7VPqtAgMBAAECggEAWG/kdkZR/u2w+9zTRf0vXX6UoDeAf+3l3MIVPam2MJhPc2yVt3weK7KtKK6tDe9TDcr4DGrvrTRwyHhdeI/xGeqBG5/NnpzeCq6Xvyqn6C/VVderNGyJvydDXgbH/YBmLh8ciGkDncNuivb8Rl2AcU9XJ7aLvYm2ufGJYDlH5WYLemp3l9gnc1YQkkGP5uH67hNYbr3njeMjrR+Ebf+FvyoVVqNGDw846/dPNNdjXmQsihiAkH3SkCW4UkEqDgV4jwHswsQxJE74KxcXCzxQK0fKsGxEHEBhICq12B2XQCMe/CyElUiZHPaFBGEYMilZvkqHLmOzKS8F0KedwDo0PQKBgQD93AwWT0yNxc7rgJ3Zp6S9IgoVAWCI/qcLKffsxej0G+hqPydPa+I9EMfxPuQix2tEmjPBF5M/aUO7Pm6FJ6KAV4eGC7lXc4yGXgru7gpU+X/ZRZLlv00J0ZPPUntxNf2dO+I7ye76bf/h/pTYG+Cxx2/FPVOXcApwmdjAIg0chwKBgQDxv8lSPx+6RtcNzJLNjAWK1eEa/xzJJIuCyD/qpZeXs1DoIWswmTt3B2y+2AqIA4L88jhV0fR10yR+14p6cmPv6qTPhCvexC7PB7K+84dwVIrvPHwy7DfYquHqABYFUzhYdlz23MunkHHFj7yhMMq7ZRYCrxwQihZquIX3oNFQKwKBgQCXphIaabjDoH0hti2Vzk3dJi57qdzvOaC0TJfse7XTzbAjeCKVkU8PR60JcpZ6KELuMmfkW5787Hcnrf370VeD78Xnz60ijsZNpnRFkzHc3UcAlk1QrdzplqcD3LQdRzF/8jwNl24JcBjB+oEUMGMkEI22eBItjyLNbTpMXIsUlQKBgFge0bLCQb1z7xYfmO+F98s41Toy/fov8Q62Ed5liBGLEpXbAKiRYoSr1y7lr0kPBqNPY8ucoaCFW4tE5DxoEBe3D2Z5RkiIeIuMwdIrERaYKqC0Co3BX0YCXCEg8OX2BNbyJkbyCuZEl8Qs9dRH5c5TNae6zt8UhsFWC7dNDp9LAoGBALaBVhLBEUGWLsAXLDMn3Cq+7KYdGCwJMucWrN3WBbvy3C25DA2QpYO2LEIHptSFzVrofMKMCZ8yPbxYoC2Wr0DX1NGgkVZmAsRDIB6wcYbWR8mu6sXq3w1ThrmOeR8VrGyDI5J41CidctOQdLlc7dI0hxq7DFMQjaEwbxbyZZUI';
+const partnerId = '217120000000451271872';
+const hostname = 'api-sd.tngdigital.com.my';
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
